efecd880553633db12aa210febd592fad612e754
alx-u <vertel@uninorte.edu.co>
2025-09-10 01:41:23 -0500

    add cocinaheroica.pro domain

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://cocinaheroica.pro'],
     credentials: true,
   });
 
